ec1440c95dcebd54f8a10ef4e52d57439f646d60
Carlin <me@carlinyuen.com>
2018-02-10 23:36:47 -0500

    transportation, accommodations, gift-registry, and our story all populated

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -48,23 +48,78 @@ function populateMainPage(pagedata) {
   }
   // END: our-story
 
-  // START: our-story
+  // START: event-details
   data = pagedata.val()['event-details'];
   if (data) {
-    container = $('#event-details');
-    data.forEach(function(section) {
-      $.each(section, function(key, value) {
+    container = $('#location');
+    $(document.createElement('h4'))
+      .text(data.location['venue-name'])
+      .appendTo(container);
+    $(document.createElement('p'))
+      .text(data.location['address'])
+      .appendTo(container);
+
+    container = $('#schedule');
+    $(document.createElement('p'))
+      .text(data.schedule['date'])
+      .appendTo(container);
+    $(document.createElement('p'))
+      .text(data.schedule['time'])
+      .appendTo(container);
+
+    container = $('#dresscode');
+    $(document.createElement('p'))
+      .text(data.dresscode)
+      .appendTo(container);
+  }
+  // END: event-details
+
+  // START: transportation
+  data = pagedata.val()['transportation'];
+  if (data) {
+    container = $('#transportation');
+    data.forEach(function(mode) {
+      $.each(mode, function(key, value) {
         $(document.createElement('h3'))
-        .text(key)
-        .appendTo(container);
-        temp = value.split('\n');
-        temp.forEach(function(text) {
-          $(document.createElement('p'))
-          .text(text)
+          .text(key)
           .appendTo(container);
-        });
+        $(document.createElement('p'))
+          .text(value)
+          .appendTo(container);
       });
     });
   }
-  // END: our-story
+  // END: transportation
+
+  // START: accommodations
+  data = pagedata.val()['accommodations'];
+  if (data) {
+    container = $('#accommodations');
+    data.forEach(function(hotel) {
+      $(document.createElement('h3'))
+        .text(hotel.name)
+        .appendTo(container);
+      $(document.createElement('p'))
+        .text(hotel.address)
+        .appendTo(container);
+      $(document.createElement('p'))
+        .text(hotel.description)
+        .appendTo(container);
+      $(document.createElement('a'))
+        .text(hotel.url)
+        .attr('href', hotel.url)
+        .appendTo(container);
+    });
+  }
+  // END: accommodations
+
+  // START: gift-registry
+  data = pagedata.val()['gift-registry'];
+  if (data) {
+    container = $('#gift-registry');
+    $(document.createElement('p'))
+      .text(data)
+      .appendTo(container);
+  }
+  // END: gift-registry
 }
