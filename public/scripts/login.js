2e6fa7f63c61311cc944856b4cc9bc5ef09eabc1
Carlin <me@carlinyuen.com>
2018-04-15 18:10:19 -0400

    merging js files

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -100,6 +100,191 @@ function enterSite() {
   loadMainPage();
 }
 
+
+// Load the main page
+function loadMainPage() {
+  if (checkAuthOrSignin()) {
+    // Fade out login page, load in data, and fade in main page
+    console.log('fireflies fade out');
+    // $('#firefly-field').addClass('blur');
+    $('#firefly-field').fadeOut('fast');
+    $('body').removeClass('gravity');
+    PAUSE_GRAVITY_SIMULATION = true;  // Pause firefly field in the background
+    $('#login-page').fadeOut('fast', function() {
+      console.log('login page fade out');
+      firebase.database().ref('content').once('value').then(populateMainPage);
+      setupParallaxIntro();
+      $('.main-page').fadeIn('fast', function() {
+        console.log('main page fade in');
+        $(window).scroll(scrollHandler);
+      });
+    });
+  }
+}
+
+// Setup parallax intro scene
+var rellax;   // Reference to rellax object
+function setupParallaxIntro() {
+  console.log('setupParallaxIntro');
+
+  /* Using rellax.min.js from Open Source */
+  if (rellax) {
+    rellax.destroy();
+  }
+  $('#parallax').fadeIn(1500, function() {
+    rellax = new Rellax('.parallax');
+  });
+
+  /* Using parallax.js from Google Developers */
+  // initializeParallax(document.querySelector('#main-page'));
+}
+
+// Scroll handler
+var BACKGROUND_GRADIENT_START_POS = 1420
+  , BACKGROUND_GRADIENT_END_POS = 1820;
+function scrollHandler(event) {
+  // console.log('scrollHandler:', event);
+
+  var scrollPos = $(this).scrollTop();
+  if (scrollPos > BACKGROUND_GRADIENT_START_POS && scrollPos < BACKGROUND_GRADIENT_END_POS) {
+    console.log('changing nav color');
+    var startColor = [255, 229, 187]
+      , endColor = [5, 11, 33]
+      , scale = (scrollPos - BACKGROUND_GRADIENT_START_POS) / (BACKGROUND_GRADIENT_END_POS - BACKGROUND_GRADIENT_START_POS)
+      , textColor = [0, 0, 0];
+    console.log(scale);
+    $.each(textColor, function(i) {
+      textColor[i] = ((endColor[i] - startColor[i]) * scale) + startColor[i];
+    });
+    console.log(textColor);
+    $('#nav > a').css('color', 'rgb('
+      + Math.round(textColor[0]) + ', '
+      + Math.round(textColor[1]) + ', '
+      + Math.round(textColor[2]) + ')');
+  }
+}
+
+// Populate main page data
+function populateMainPage(pagedata) {
+  console.log('populateMainPage:', pagedata.val());
+
+  // Sanity check
+  if (!pagedata.val()) {
+    return alert('Could not load data! Please let us know if you see this error.');
+  }
+  var data, container, temp;
+
+  // START: our-story
+  data = pagedata.val()['our-story'];
+  if (data) {
+    container = $('#our-story');
+    data.forEach(function(section) {
+      $.each(section, function(key, value) {
+        $(document.createElement('h3'))
+          .addClass('main-page__data')
+          .text(key)
+          .appendTo(container);
+        temp = value.split('\n');
+        temp.forEach(function(text) {
+          $(document.createElement('p'))
+            .addClass('main-page__data')
+            .text(text)
+            .appendTo(container);
+        });
+      });
+    });
+  }
+  // END: our-story
+
+  // START: event-details
+  data = pagedata.val()['event-details'];
+  if (data) {
+    container = $('#location');
+    $(document.createElement('p'))
+      .addClass('main-page__data')
+      .text(data.location['venue-name'])
+      .appendTo(container);
+    $(document.createElement('p'))
+      .addClass('main-page__data')
+      .text(data.location['address'])
+      .appendTo(container);
+
+    container = $('#schedule');
+    $(document.createElement('p'))
+      .addClass('main-page__data')
+      .text(data.schedule['date'])
+      .appendTo(container);
+    $(document.createElement('p'))
+      .addClass('main-page__data')
+      .text(data.schedule['time'])
+      .appendTo(container);
+
+    container = $('#dresscode');
+    $(document.createElement('p'))
+      .addClass('main-page__data')
+      .text(data.dresscode)
+      .appendTo(container);
+  }
+  // END: event-details
+
+  // START: transportation
+  data = pagedata.val()['transportation'];
+  if (data) {
+    container = $('#transportation');
+    data.forEach(function(mode) {
+      $.each(mode, function(key, value) {
+        $(document.createElement('h3'))
+          .addClass('main-page__data')
+          .text(key)
+          .appendTo(container);
+        $(document.createElement('p'))
+          .addClass('main-page__data')
+          .text(value)
+          .appendTo(container);
+      });
+    });
+  }
+  // END: transportation
+
+  // START: accommodations
+  data = pagedata.val()['accommodations'];
+  if (data) {
+    container = $('#accommodations');
+    data.forEach(function(hotel) {
+      $(document.createElement('h3'))
+        .addClass('main-page__data')
+        .text(hotel.name)
+        .appendTo(container);
+      $(document.createElement('p'))
+        .addClass('main-page__data')
+        .text(hotel.address)
+        .appendTo(container);
+      $(document.createElement('p'))
+        .addClass('main-page__data')
+        .text(hotel.description)
+        .appendTo(container);
+      $(document.createElement('a'))
+        .addClass('main-page__data')
+        .text(hotel.url)
+        .attr('href', hotel.url)
+        .attr('target', '_blank')
+        .appendTo(container);
+    });
+  }
+  // END: accommodations
+
+  // START: gift-registry
+  data = pagedata.val()['gift-registry'];
+  if (data) {
+    container = $('#gift-registry');
+    $(document.createElement('p'))
+      .addClass('main-page__data')
+      .html(data.split('\n').join('<br>'))
+      .appendTo(container);
+  }
+  // END: gift-registry
+}
+
 /**
 * Sends password reset/change email to email account
 */
@@ -220,11 +405,14 @@ function initApp() {
         scrollTop: $($(event.target).attr('href')).offset().top
     }, 1000);
   });
-  $('#signin-button').on('click', signIn);
-  $('.signout-button').on('click', signOut);
-  $('#entersite-button').on('click', enterSite);
-  $('.changepassword-button').on('click', sendPasswordReset);
+  $('#signin-button').click(signIn);
+  $('.signout-button').click(signOut);
+  $('#entersite-button').click(enterSite);
+  $('.changepassword-button').click(sendPasswordReset);
   $('#login-form').submit(signIn);
+  $('#title').click(function() {
+    $(this).toggleClass('expanded');
+  });
 }
 
 window.onload = function() {
