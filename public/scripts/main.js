4eb5f96570eb656a56d48ff7090dba76398e9a3e
Carlin <me@carlinyuen.com>
2018-05-05 02:29:55 -0400

    more styling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,59 @@
+// requestAnimationFrame() shim by Paul Irish
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function() {
+	return  window.requestAnimationFrame       ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+/**
+ * https://gist.github.com/joelambert/1002116
+ * Behaves the same as setTimeout except uses requestAnimationFrame() where possible for better performance
+ * @param {function} fn The callback function
+ * @param {int} delay The delay in milliseconds
+ */
+window.requestTimeout = function(fn, delay) {
+	if( !window.requestAnimationFrame      	&&
+		!window.webkitRequestAnimationFrame &&
+		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+		!window.oRequestAnimationFrame      &&
+		!window.msRequestAnimationFrame)
+			return window.setTimeout(fn, delay);
+
+	var start = new Date().getTime(),
+		handle = new Object();
+
+	function loop(){
+		var current = new Date().getTime(),
+			delta = current - start;
+
+		delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+	};
+
+	handle.value = requestAnimFrame(loop);
+	return handle;
+};
+
+/**
+ * Behaves the same as clearTimeout except uses cancelRequestAnimationFrame() where possible for better performance
+ * @param {int|object} fn The callback function
+ */
+window.clearRequestTimeout = function(handle) {
+    window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+    window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
+    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) : /* Support for legacy API */
+    window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+    window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
+    window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
+    clearTimeout(handle);
+};
+
+
 (function () {
   'use strict';
 
@@ -19,7 +75,7 @@
       if (!wait) {                    // If we're not waiting
         callback.call();              // Execute users function
         wait = true;                  // Prevent future invocations
-        setTimeout(function () {      // After a period of time
+        requestTimeout(function () {      // After a period of time
           wait = false;               // And allow future invocations
         }, limit);
       }
@@ -192,7 +248,7 @@
             scrollListenerID = requestAnimationFrame(scrollHandler);  // Add scroll position listener
             setupFlickityCarousel();
             setupPhotoSwipe();
-            setTimeout(function() {
+            requestTimeout(function() {
               $('#title').toggleClass('expanded', false);
               $flickity.flickity('resize');
             }, TIME_DURATION_XL);
@@ -1238,7 +1294,7 @@
 
   window.onload = function() {
     initApp();
-    setTimeout(function() {
+    requestTimeout(function() {
       var email = getURLParam('email');
       if (email) {
         $('#email-input').val(email);
